425e7f7fb7b4f1ec3bd584fc9a336955388a1a02
liamhoganson <93838459+liamhoganson@users.noreply.github.com>
2023-01-11 09:29:45 -0700

    Update printeradder.py

--- printeradder.py
+++ printeradder.py
@@ -1,1 +1,17 @@
+from officeprinteradder import office_printer_adder
+from jobsiteprinteradder import jobsite_printer_selection
 
+if __name__ == '__main__':
+	# Asks user if jobsite or office printer (Main selection)
+	while True:
+		try:
+			ask_type = (int(input("What kind of printer is this? \n(1) Office Printer \n(2) Jobsite Printer \nEnter answer here:")))
+			if ask_type == 1:
+				office_printer_adder()
+			elif ask_type == 2:
+				jobsite_printer_selection()
+			elif ask_type not in (1, 2):
+				print("Invalid Input!")
+				continue
+		except ValueError as err:
+			print("Invalid Input: {}".format(err))
